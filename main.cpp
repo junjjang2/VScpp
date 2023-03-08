13e6672c4825901035ac1be8c54d8ac9ffb1470f
DESKTOP-078QAVR\82103 <junjjang2@khu.ac.kr>
2023-03-08 12:59:43 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+
+// unique main function in this project
+// other programs can be executed through it's sub_main() function
+int main() {
+	cout << "hello, world!" << endl;
+	
+	return 0;
+}
